6bc5e996139112567cfb9bbd9682bd6a710af992
Norual <jvmlauron@outlook.com>
2017-04-12 10:59:25 +0800

    Change Highlight Image
    Change Hightlight Image from just turning it to green to having  small
    circles around the circumference.

--- Globals.vb
+++ Globals.vb
@@ -4,5 +4,6 @@
         State
     End Enum
 
-    Public Const CircleDiam As Integer = 60
+    Public Const CircleDiam As Integer = 60 'actual diameter - 1. 
+    Public Const Radius As Integer = CInt(CircleDiam / 2)
 End Module
--- State.vb
+++ State.vb
@@ -55,16 +55,16 @@
 
                     Dim pbx As PictureBox = CType(sender, PictureBox)
                     If Not m_bMouseDown Then
-                        Dim a As Integer = Math.Abs(m_pLocation.X - e.Location.X)
-                        Dim b As Integer = Math.Abs(m_pLocation.Y - e.Location.Y)
+                        Dim pLocation As New Point(m_pLocation.X + Math.Floor(m_bmpCircle.Width / 2), m_pLocation.Y + Math.Floor(m_bmpCircle.Height / 2))
+                        Dim a As Integer = Math.Abs(pLocation.X - e.Location.X)
+                        Dim b As Integer = Math.Abs(pLocation.Y - e.Location.Y)
                         Dim hypotenuse As Integer = Math.Sqrt((a ^ 2) + (b ^ 2))
-                        Dim radius As Integer = CircleDiam / 2
-                        If hypotenuse < radius And Not m_bMouseEntered Then
+                        If hypotenuse < Radius + 1 And Not m_bMouseEntered Then
                             RaiseEvent MouseEntered(Me)
                             m_pbxClone = BackImage(pbx)
                             HighlightState(pbx, m_pLocation)
                             m_bMouseEntered = True
-                        ElseIf hypotenuse > radius And m_bMouseEntered Then
+                        ElseIf hypotenuse > Radius + 4 And m_bMouseEntered Then
                             pbx.Image = m_pbxClone
                             m_bMouseEntered = False
                             RaiseEvent MouseLeave()
@@ -86,7 +86,7 @@
                 If m_bMouseEntered Then
                     If Not m_bMouseDown Then
                         Dim pbx As PictureBox = CType(sender, PictureBox)
-                        m_pDiff = New Point(e.Location.X - (m_pLocation.X - CInt(m_bmpCircle.Width / 2)), e.Location.Y - (m_pLocation.Y - CInt(m_bmpCircle.Height / 2)))
+                        m_pDiff = New Point(e.Location.X - m_pLocation.X, e.Location.Y - m_pLocation.Y)
                         RaiseEvent Refresh(Me)
                         m_pbxClone = BackImage(pbx)
                         MoveState(pbx, e.Location)
@@ -128,9 +128,8 @@
     Private Sub MoveState(ByVal pbx As PictureBox, ByVal p As Point)
         Dim bmp As Bitmap = BackImage(pbx, True)
         Dim g As Graphics = Graphics.FromImage(bmp)
-        Dim pLocation As New Point(p.X - m_pDiff.X, p.Y - m_pDiff.Y)
-        m_pLocation = New Point(p.X - m_pDiff.X + CInt(m_bmpCircle.Width / 2), p.Y - m_pDiff.Y + CInt(m_bmpCircle.Height / 2))
-        g.DrawImage(m_bmpCircle, pLocation)
+        m_pLocation = New Point(p.X - m_pDiff.X, p.Y - m_pDiff.Y)
+        g.DrawImage(m_bmpCircle, m_pLocation)
         m_bmpImage = bmp.Clone
         g.DrawImage(m_pbxClone, 0, 0)
         g.DrawImage(m_bmpImage, 0, 0)
@@ -142,8 +141,7 @@
     Private Sub HighlightState(ByVal pbx As PictureBox, ByVal p As Point)
         Dim bmp As Bitmap = BackImage(pbx)
         Dim g As Graphics = Graphics.FromImage(bmp)
-        Dim pLocation As New Point(CInt(p.X - (m_bmpCircle.Width / 2)), CInt(p.Y - (m_bmpCircle.Height / 2)))
-        g.DrawImage(m_bmpHighlightedCircle, pLocation)
+        g.DrawImage(m_bmpHighlightedCircle, m_pLocation)
         pbx.Image = bmp.Clone
         bmp.Dispose()
         g.Dispose()
@@ -152,15 +150,12 @@
     Private Sub DrawState(ByVal pbx As PictureBox, ByVal p As Point)
         Dim bmp As New Bitmap(pbx.Width, pbx.Height)
         Dim g As Graphics = Graphics.FromImage(bmp)
-        Dim pLocation As New Point(CInt(p.X - (m_bmpCircle.Width / 2)), CInt(p.Y - (m_bmpCircle.Height / 2)))
-        m_pLocation = p
-        g.DrawImage(m_bmpCircle, pLocation)
+        m_pLocation = New Point(p.X - Math.Floor(m_bmpCircle.Width / 2), p.Y - Math.Floor(m_bmpCircle.Height / 2))
+        g.DrawImage(m_bmpCircle, m_pLocation)
         m_bmpImage = bmp.Clone
         bmp.Dispose()
         g.Dispose()
     End Sub
-
-
 
     Private Function BackImage(ByVal pbx As PictureBox, Optional bForceCreateNew As Boolean = False) As Bitmap
         If pbx.Image Is Nothing Or bForceCreateNew Then
